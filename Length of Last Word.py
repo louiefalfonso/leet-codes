ac9e3164fc6b871a543b889878b663fc3ee6aa03
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-03 22:07:04 +0100

    completed leet codes
    completed leet codes

--- Length of Last Word.py
+++ Length of Last Word.py
@@ -9,23 +9,15 @@ Explanation: The last word is "World" with length 5.
 
 
 def lengthOfLastWord(s: str) -> int:
-    """
-    :type s: str
-    :rtype: int
-    """
-    i = len(s) - 1
     length = 0
-
-    while i >= 0 and s[i] == " ":
+    i = len(s) - 1
+    while i >= 0 and s[i] == ' ':
         i -= 1
-
-    while i >= 0 and s[i] != " ":
+    while i >= 0 and s[i] != ' ':
         length += 1
         i -= 1
-
     return length
 
 
-class Solution(object):
+class Solution:
     pass
-
