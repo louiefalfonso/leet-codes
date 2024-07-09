2487edffa8f0e8bd8a5d21cae62c8f87408b6d31
Louie Alfonso <lfriasalfonso@gmail.com>
2024-07-09 14:32:01 +0100

    Leet Code for July-9-2024
    Leet Code for July-9-2024

---
+++ python/GreatestCommonDivisorofStrings.py
@@ -0,0 +1,12 @@
+#1071. Greatest Common Divisor of Strings
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        if len(str1) < len(str2):
+            str1, str2 = str2, str1
+        if str1 == str2:
+            return str1
+        if str1[:len(str2)] != str2:
+            return ""
+        return self.gcdOfStrings(str1[len(str2):], str2)
+
