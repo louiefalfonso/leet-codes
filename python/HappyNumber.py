b91a30bab295bb67e80d734763858e6f1a06ba99
Louie Alfonso <lfriasalfonso@gmail.com>
2025-01-28 21:31:40 +0000

    Leet Code Jan 28 2025

--- python/HappyNumber.py
+++
@@ -1,9 +0,0 @@
-# 202. Happy Number
-
-def is_happy(n: int) -> bool:
-   
-    seen = set()  
-    while n != 1 and n not in seen:
-        seen.add(n)
-        n = sum(int(digit) ** 2 for digit in str(n)) 
-    return n == 1
