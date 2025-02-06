d60ab5f32724363b719cef38ab2fd779a3e5fc9b
Louie Alfonso <lfriasalfonso@gmail.com>
2025-02-06 14:21:49 +0000

    Leet Code Feb 06 2025

---
+++ java/Tuple with Same Product.java
@@ -0,0 +1,21 @@
+// Tuple with Same Product - 1726
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int i = 1; i < nums.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int x = nums[i] * nums[j];
+                cnt.merge(x, 1, Integer::sum);
+            }
+        }
+        int ans = 0;
+        for (int v : cnt.values()) {
+            ans += v * (v - 1) / 2;
+        }
+        return ans << 3;
+    }
+}
