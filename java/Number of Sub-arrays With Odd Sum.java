f6529f5a293c964b46fe7bb8beafb339f69bfcdb
Louie Alfonso <lfriasalfonso@gmail.com>
2025-02-25 18:01:47 +0000

    Leet Code Feb 25 2025

---
+++ java/Number of Sub-arrays With Odd Sum.java
@@ -0,0 +1,16 @@
+// Number of Sub-arrays With Odd Sum - 1524
+
+
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        final int mod = (int) 1e9 + 7;
+        int[] cnt = { 1, 0 };
+        int ans = 0, s = 0;
+        for (int x : arr) {
+            s += x;
+            ans = (ans + cnt[s & 1 ^ 1]) % mod;
+            ++cnt[s & 1];
+        }
+        return ans;
+    }
+}
