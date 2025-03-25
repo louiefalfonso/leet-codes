f11f8098f20f821d383e2b3c73c25c84d41062f9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-25 19:40:24 +0000

    Leetcode Mar 25 2025

---
+++ java/Updated Attendance Components & Services.java
@@ -0,0 +1,18 @@
+// Updated Attendance Components & Services - 3169
+import java.util.*;
+
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
+        int ans = 0, last = 0;
+        for (var e : meetings) {
+            int st = e[0], ed = e[1];
+            if (last < st) {
+                ans += st - last - 1;
+            }
+            last = Math.max(last, ed);
+        }
+        ans += days - last;
+        return ans;
+    }
+}
