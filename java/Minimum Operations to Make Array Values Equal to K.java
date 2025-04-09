fc18d5c21aaf5d7314d9aaf4382a1bc6caedccd7
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-09 23:17:16 +0100

    Leetcode April 09 2025

---
+++ java/Minimum Operations to Make Array Values Equal to K.java
@@ -0,0 +1,18 @@
+// Minimum Operations to Make Array Values Equal to K  - 3375
+
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Set<Integer> s = new HashSet<>();
+        int mi = 1 << 30;
+        for (int x : nums) {
+            if (x < k) {
+                return -1;
+            }
+            mi = Math.min(mi, x);
+            s.add(x);
+        }
+        return s.size() - (mi == k ? 1 : 0);
+    }
+}
