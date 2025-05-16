73b84489ceafee7ee5399a9a6e3027c0de510b01
Louie Alfonso <lfriasalfonso@gmail.com>
2025-05-16 14:24:44 +0100

    Leetcode May 16 2025

---
+++ java/Longest Unequal Adjacent Groups Subsequence I.java
@@ -0,0 +1,16 @@
+// Longest Unequal Adjacent Groups Subsequence I - 2900
+
+import java.util.*;
+
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        int n = groups.length;
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.add(words[i]);
+            }
+        }
+        return ans;
+    }
+}
