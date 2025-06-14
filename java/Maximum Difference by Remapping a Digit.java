74dd5eabf12bd947e94a6949acaaf8a797f703b9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-06-14 17:03:01 +0100

    Leetcode June 14 2025

---
+++ java/Maximum Difference by Remapping a Digit.java
@@ -0,0 +1,14 @@
+// Maximum Difference by Remapping a Digit - 2566
+
+class Solution {
+    public int minMaxDifference(int num) {
+        String s = String.valueOf(num);
+        int mi = Integer.parseInt(s.replace(s.charAt(0), '0'));
+        for (char c : s.toCharArray()) {
+            if (c != '9') {
+                return Integer.parseInt(s.replace(c, '9')) - mi;
+            }
+        }
+        return num - mi;
+    }
+}
