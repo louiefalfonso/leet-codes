60f99b97d16f6e377a7dcee5fe982cf720c93dd9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-06-16 13:54:33 +0100

    Leetcode June 16 2025

---
+++ java/Maximum Difference Between Increasing Elements.java
@@ -0,0 +1,16 @@
+// Maximum Difference Between Increasing Elements - 2016
+
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int mi = 1 << 30;
+        int ans = -1;
+        for (int x : nums) {
+            if (x > mi) {
+                ans = Math.max(ans, x - mi);
+            } else {
+                mi = x;
+            }
+        }
+        return ans;
+    }
+}
