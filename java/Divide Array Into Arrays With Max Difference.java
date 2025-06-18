b3bb5a6404af1c8458b3c2d24d8d96d3ab168db0
Louie Alfonso <lfriasalfonso@gmail.com>
2025-06-18 21:47:43 +0100

    Leetcode June 18 2025

---
+++ java/Divide Array Into Arrays With Max Difference.java
@@ -0,0 +1,17 @@
+// Divide Array Into Arrays With Max Difference - 2966
+import java.util.*;
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[][] ans = new int[n / 3][];
+        for (int i = 0; i < n; i += 3) {
+            int[] t = Arrays.copyOfRange(nums, i, i + 3);
+            if (t[2] - t[0] > k) {
+                return new int[][] {};
+            }
+            ans[i / 3] = t;
+        }
+        return ans;
+    }
+}
