84f73ee07b0f8ffbe7f7e0139fd144ec4e07b523
Louie Alfonso <lfriasalfonso@gmail.com>
2025-06-22 21:28:33 +0100

    Leetcode June 22 2025

---
+++ java/Divide a String Into Groups of Size k.java
@@ -0,0 +1,15 @@
+// Divide a String Into Groups of Size k - 2138
+
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        int n = s.length();
+        String[] ans = new String[(n + k - 1) / k];
+        if (n % k != 0) {
+            s += String.valueOf(fill).repeat(k - n % k);
+        }
+        for (int i = 0; i < ans.length; ++i) {
+            ans[i] = s.substring(i * k, (i + 1) * k);
+        }
+        return ans;
+    }
+}
