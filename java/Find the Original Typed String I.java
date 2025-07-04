3dfbcdb43cab31fee51930a7263558c4466c5455
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-04 18:50:49 +0100

    Leetcode July 04 2025

---
+++ java/Find the Original Typed String I.java
@@ -0,0 +1,13 @@
+// Find the Original Typed String I - 3330
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int f = 1;
+        for (int i = 1; i < word.length(); ++i) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                ++f;
+            }
+        }
+        return f;
+    }
+}
