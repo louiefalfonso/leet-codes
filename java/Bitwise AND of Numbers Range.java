a20957466d646118b04bad3bf2d3239cdef2e96c
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-06 21:55:01 +0100

    Leetcode July 06 2025

---
+++ java/Bitwise AND of Numbers Range.java
@@ -0,0 +1,10 @@
+// Bitwise AND of Numbers Range - 201
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        while (left < right) {
+            right &= (right - 1);
+        }
+        return right;
+    }
+}
