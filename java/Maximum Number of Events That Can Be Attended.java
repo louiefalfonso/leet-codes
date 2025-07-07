163ea811b204a6c34ff86c482026e610807a934e
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-07 22:09:29 +0100

    Leetcode July 07 2025

---
+++ java/Maximum Number of Events That Can Be Attended.java
@@ -0,0 +1,31 @@
+// Maximum Number of Events That Can Be Attended - 1353
+
+ import java.util.*;
+
+class Solution {
+    public int maxEvents(int[][] events) {
+        Map<Integer, List<Integer>> g = new HashMap<>();
+        int l = Integer.MAX_VALUE, r = 0;
+        for (int[] event : events) {
+            int s = event[0], e = event[1];
+            g.computeIfAbsent(s, k -> new ArrayList<>()).add(e);
+            l = Math.min(l, s);
+            r = Math.max(r, e);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int ans = 0;
+        for (int s = l; s <= r; s++) {
+            while (!pq.isEmpty() && pq.peek() < s) {
+                pq.poll();
+            }
+            for (int e : g.getOrDefault(s, List.of())) {
+                pq.offer(e);
+            }
+            if (!pq.isEmpty()) {
+                pq.poll();
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
