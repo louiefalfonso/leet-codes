8f14a4c644a4465c8013a88e680ac9347aedb0f0
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-17 12:08:04 +0100

    Leetcode July 17 2025

---
+++ java/Find the Maximum Length of Valid Subsequence II.java
@@ -0,0 +1,17 @@
+// Find the Maximum Length of Valid Subsequence II -3202
+
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        int[][] f = new int[k][k];
+        int ans = 0;
+        for (int x : nums) {
+            x %= k;
+            for (int j = 0; j < k; ++j) {
+                int y = (j - x + k) % k;
+                f[x][y] = f[y][x] + 1;
+                ans = Math.max(ans, f[x][y]);
+            }
+        }
+        return ans;
+    }
+}
