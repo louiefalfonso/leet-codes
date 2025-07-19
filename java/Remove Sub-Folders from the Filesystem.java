98ab45eac2d8b5baa1014a323a04781b69ac5886
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-19 22:40:10 +0100

    Leetcode July 19 2025

---
+++ java/Remove Sub-Folders from the Filesystem.java
@@ -0,0 +1,23 @@
+// Remove Sub-Folders from the Filesystem - 1233
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> ans = new ArrayList<>();
+        ans.add(folder[0]);
+        for (int i = 1; i < folder.length; ++i) {
+            int m = ans.get(ans.size() - 1).length();
+            int n = folder[i].length();
+            if (m >= n
+                || !(ans.get(ans.size() - 1).equals(folder[i].substring(0, m))
+                    && folder[i].charAt(m) == '/')) {
+                ans.add(folder[i]);
+            }
+        }
+        return ans;
+    }
+}
