07526db523e3f501fee7ad2653608911288e9052
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-25 05:33:24 +0100

    Leetcode July 25 2025

---
+++ java/Maximum Unique Subarray Sum After Deletion.java
@@ -0,0 +1,22 @@
+// Maximum Unique Subarray Sum After Deletion - 3487
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxSum(int[] nums) {
+        int mx = Arrays.stream(nums).max().getAsInt();
+        if (mx <= 0) {
+            return mx;
+        }
+        boolean[] s = new boolean[201];
+        int ans = 0;
+        for (int x : nums) {
+            if (x < 0 || s[x]) {
+                continue;
+            }
+            ans += x;
+            s[x] = true;
+        }
+        return ans;
+    }
+}
