eafb0b0e70a400de54fd7c75b4a6253f801a42fa
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-30 18:09:00 +0100

    Leetcode July 30 2025

---
+++ java/Longest Subarray With Maximum Bitwise AND.java
@@ -0,0 +1,18 @@
+// Longest Subarray With Maximum Bitwise AND - 2419
+
+import java.util.Arrays;
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int mx = Arrays.stream(nums).max().getAsInt();
+        int ans = 0, cnt = 0;
+        for (int x : nums) {
+            if (x == mx) {
+                ans = Math.max(ans, ++cnt);
+            } else {
+                cnt = 0;
+            }
+        }
+        return ans;
+    }
+}
