040cbf44c14056c14ec9cbd610e2af6980a78fe4
Louie Alfonso <lfriasalfonso@gmail.com>
2025-08-01 20:12:52 +0100

    Leetcode Aug 01 2025

---
+++ java/Pascal's Triangle.java
@@ -0,0 +1,21 @@
+// Pascal's Triangle - 118
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> f = new ArrayList<>();
+        f.add(List.of(1));
+        for (int i = 0; i < numRows - 1; ++i) {
+            List<Integer> g = new ArrayList<>();
+            g.add(1);
+            for (int j = 1; j < f.get(i).size(); ++j) {
+                g.add(f.get(i).get(j - 1) + f.get(i).get(j));
+            }
+            g.add(1);
+            f.add(g);
+        }
+        return f;
+    }
+}
