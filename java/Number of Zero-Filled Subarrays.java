6f647821bca9432ddb17fb1280102edc192328be
Louie Alfonso <lfriasalfonso@gmail.com>
2025-08-19 19:24:25 +0100

    Leetcode Aug 19 2025

---
+++ java/Number of Zero-Filled Subarrays.java
@@ -0,0 +1,16 @@
+// Number of Zero-Filled Subarrays - 2348
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long ans = 0;
+        int cnt = 0;
+        for (int x : nums) {
+            if (x == 0) {
+                ans += ++cnt;
+            } else {
+                cnt = 0;
+            }
+        }
+        return ans;
+    }
+}
