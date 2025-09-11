054b0152400f9946d54c876a6e982d84d32a1148
Louie Alfonso <lfriasalfonso@gmail.com>
2025-09-11 18:57:18 +0100

    Leetcode Sept 11 2025

---
+++ java/Sort Vowels in a String.java
@@ -0,0 +1,25 @@
+// Sort Vowels in a String - 2785
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> vs = new ArrayList<>();
+        char[] cs = s.toCharArray();
+        for (char c : cs) {
+            char d = Character.toLowerCase(c);
+            if (d == 'a' || d == 'e' || d == 'i' || d == 'o' || d == 'u') {
+                vs.add(c);
+            }
+        }
+        Collections.sort(vs);
+        for (int i = 0, j = 0; i < cs.length; ++i) {
+            char d = Character.toLowerCase(cs[i]);
+            if (d == 'a' || d == 'e' || d == 'i' || d == 'o' || d == 'u') {
+                cs[i] = vs.get(j++);
+            }
+        }
+        return String.valueOf(cs);
+    }
+}
