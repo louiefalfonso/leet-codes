2e0db86a99ba4da7f4913b0e531a4d998149d883
Louie Alfonso <lfriasalfonso@gmail.com>
2025-09-12 20:37:21 +0100

    Leetcode Sept 12 2025

---
+++ java/Vowels Game in a String.java
@@ -0,0 +1,12 @@
+// Vowels Game in a String - 3227
+
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+            if ("aeiou".indexOf(s.charAt(i)) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
