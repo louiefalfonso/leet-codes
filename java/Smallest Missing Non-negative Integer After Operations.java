af463d584114e181fd652d20f5621c37c6cf1e81
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-16 19:49:41 +0100

    Leetcode Oct 16 2025

---
+++ java/Smallest Missing Non-negative Integer After Operations.java
@@ -0,0 +1,14 @@
+// Smallest Missing Non-negative Integer After Operations - 2598
+class Solution {
+    public int findSmallestInteger(int[] nums, int value) {
+        int[] cnt = new int[value];
+        for (int x : nums) {
+            ++cnt[(x % value + value) % value];
+        }
+        for (int i = 0;; ++i) {
+            if (cnt[i % value]-- == 0) {
+                return i;
+            }
+        }
+    }
+}
