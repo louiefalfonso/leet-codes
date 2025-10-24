0e7c53ca4fa9d82e71b38af4f2c8355fd4cc6b41
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-24 15:25:55 +0100

    Leetcode Oct 24 2025

---
+++ python/Next Greater Numerically Balanced Number.py
@@ -0,0 +1,12 @@
+# Next Greater Numerically Balanced Number - 2048
+
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        for x in count(n + 1):
+            y = x
+            cnt = [0] * 10
+            while y:
+                y, v = divmod(y, 10)
+                cnt[v] += 1
+            if all(v == 0 or i == v for i, v in enumerate(cnt)):
+                return x
