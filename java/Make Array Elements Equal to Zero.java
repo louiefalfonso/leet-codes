bd523e382f01c5366ce802bfe6e3e42d1a259e63
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-28 21:34:49 +0000

    Leetcode Oct 28 2025

---
+++ java/Make Array Elements Equal to Zero.java
@@ -0,0 +1,20 @@
+// Make Array Elements Equal to Zero - 3354
+
+import java.util.Arrays;
+
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int s = Arrays.stream(nums).sum();
+        int ans = 0, l = 0;
+        for (int x : nums) {
+            if (x != 0) {
+                l += x;
+            } else if (l * 2 == s) {
+                ans += 2;
+            } else if (Math.abs(l * 2 - s) <= 1) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
