925bb1a1d019a0f7ae098490f8e86d817de306b5
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-29 19:33:31 +0000

    Leetcode Oct 29 2025

---
+++ java/Smallest Number With All Set Bits.java
@@ -0,0 +1,11 @@
+// Smallest Number With All Set Bits - 3370
+
+class Solution {
+    public int smallestNumber(int n) {
+        int x = 1;
+        while (x - 1 < n) {
+            x <<= 1;
+        }
+        return x - 1;
+    }
+}
---
+++ python/Smallest Number With All Set Bits.py
@@ -0,0 +1,8 @@
+# Smallest Number With All Set Bits - 3370
+
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        x = 1
+        while x - 1 < n:
+            x <<= 1
+        return x - 1
