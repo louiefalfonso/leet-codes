07c60d18997ae628ce7d6b367c09d48e5d1941a2
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-30 19:46:40 +0000

    Leetcode Oct 30 2025

---
+++ java/Minimum Number of Increments on Subarrays to Form a Target Array.java
@@ -0,0 +1,13 @@
+// Minimum Number of Increments on Subarrays to Form a Target Array - 1526
+
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int f = target[0];
+        for (int i = 1; i < target.length; ++i) {
+            if (target[i] > target[i - 1]) {
+                f += target[i] - target[i - 1];
+            }
+        }
+        return f;
+    }
+}
