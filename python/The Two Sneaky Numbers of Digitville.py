956296b24c29dfd517ccea97298581d0f6b4c5e9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-31 20:07:49 +0000

    Leetcode Oct 31 2025

---
+++ python/The Two Sneaky Numbers of Digitville.py
@@ -0,0 +1,6 @@
+# The Two Sneaky Numbers of Digitville - 3289
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+        return [x for x, v in cnt.items() if v == 2]
