a07642033111decc40977a812de5bd92052fedd9
Louie Alfonso <lfriasalfonso@gmail.com>
2025-11-14 18:52:09 +0000

    Leetcode Nov 14 2025

--- python/Maximum Number of Operations to Move Ones to the End.py
+++ python/Maximum Number of Operations to Move Ones to the End.py
@@ -1,1 +1,11 @@
 # Maximum Number of Operations to Move Ones to the End - 3228
+
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        ans = cnt = 0
+        for i, c in enumerate(s):
+            if c == "1":
+                cnt += 1
+            elif i and s[i - 1] == "1":
+                ans += cnt
+        return ans
