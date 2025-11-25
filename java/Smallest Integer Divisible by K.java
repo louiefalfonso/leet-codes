72c43b6b63695a48cc46d3e94fadb45770706151
Louie Alfonso <lfriasalfonso@gmail.com>
2025-11-25 20:07:18 +0000

    Leetcode Nov 25 2025

---
+++ java/Smallest Integer Divisible by K.java
@@ -0,0 +1,14 @@
+// Smallest Integer Divisible by K - 1015
+
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        int n = 1 % k;
+        for (int i = 1; i <= k; ++i) {
+            if (n == 0) {
+                return i;
+            }
+            n = (n * 10 + 1) % k;
+        }
+        return -1;
+    }
+}
---
+++ python/Smallest Integer Divisible by K.py
@@ -0,0 +1,10 @@
+# Smallest Integer Divisible by K - 1015
+
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        n = 1 % k
+        for i in range(1, k + 1):
+            if n == 0:
+                return i
+            n = (n * 10 + 1) % k
+        return -1
