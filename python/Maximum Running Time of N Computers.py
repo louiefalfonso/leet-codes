8fd855baf35c57da5febb5546aa757561e4caf8d
Louie Alfonso <lfriasalfonso@gmail.com>
2025-12-01 19:26:43 +0000

    Leetcode Dec 01 2025

---
+++ python/Maximum Running Time of N Computers.py
@@ -0,0 +1,12 @@
+# Maximum Running Time of N Computers - 2141
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        l, r = 0, sum(batteries)
+        while l < r:
+            mid = (l + r + 1) >> 1
+            if sum(min(x, mid) for x in batteries) >= n * mid:
+                l = mid
+            else:
+                r = mid - 1
+        return l
