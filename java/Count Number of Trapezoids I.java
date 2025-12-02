d06af6e08f3cca61db00f915ac5a9f24f7a687e0
Louie Alfonso <lfriasalfonso@gmail.com>
2025-12-02 20:20:00 +0000

    Leetcode Dec 02 2025

---
+++ java/Count Number of Trapezoids I.java
@@ -0,0 +1,20 @@
+// Count Number of Trapezoids I - 3623
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int countTrapezoids(int[][] points) {
+        final int mod = (int) 1e9 + 7;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (var p : points) {
+            cnt.merge(p[1], 1, Integer::sum);
+        }
+        long ans = 0, s = 0;
+        for (int v : cnt.values()) {
+            long t = 1L * v * (v - 1) / 2;
+            ans = (ans + s * t) % mod;
+            s += t;
+        }
+        return (int) ans;
+    }
+}
