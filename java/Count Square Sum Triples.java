2a05c043b91f3a9dfe4fa306e3877070d870e1ee
Louie Alfonso <lfriasalfonso@gmail.com>
2025-12-08 18:03:23 +0000

    Leetcode Dec 07 2025

---
+++ java/Count Square Sum Triples.java
@@ -0,0 +1,17 @@
+// Count Square Sum Triples - 1925
+
+class Solution {
+    public int countTriples(int n) {
+        int ans = 0;
+        for (int a = 1; a < n; a++) {
+            for (int b = 1; b < n; b++) {
+                int x = a * a + b * b;
+                int c = (int) Math.sqrt(x);
+                if (c <= n && c * c == x) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ python/Count Square Sum Triples.py
@@ -0,0 +1,12 @@
+# Count Square Sum Triples - 1925
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        ans = 0
+        for a in range(1, n):
+            for b in range(1, n):
+                x = a * a + b * b
+                c = int(sqrt(x))
+                if c <= n and c * c == x:
+                    ans += 1
+        return ans
