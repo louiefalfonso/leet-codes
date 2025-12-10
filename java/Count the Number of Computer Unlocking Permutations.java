53e0129ca876da5c704984e3fd7048571293ff10
Louie Alfonso <lfriasalfonso@gmail.com>
2025-12-10 19:02:57 +0000

    Leetcode Dec 10 2025

---
+++ java/Count the Number of Computer Unlocking Permutations.java
@@ -0,0 +1,15 @@
+// Count the Number of Computer Unlocking Permutations - 3577
+
+class Solution {
+    public int countPermutations(int[] complexity) {
+        final int mod = (int) 1e9 + 7;
+        long ans = 1;
+        for (int i = 1; i < complexity.length; ++i) {
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            }
+            ans = ans * i % mod;
+        }
+        return (int) ans;
+    }
+}
