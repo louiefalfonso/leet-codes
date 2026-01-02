d6144e8d110cfc0471128a2a876b4c36db6c6ff3
Louie Alfonso <lfriasalfonso@gmail.com>
2026-01-02 16:01:45 +0000

    Leetcode Jan 02 2026

---
+++ java/N-Repeated Element in Size 2N Array.java
@@ -0,0 +1,15 @@
+// N-Repeated Element in Size 2N Array - 961
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> s = new HashSet<>(nums.length / 2 + 1);
+        for (int i = 0;; ++i) {
+            if (!s.add(nums[i])) {
+                return nums[i];
+            }
+        }
+    }
+}
