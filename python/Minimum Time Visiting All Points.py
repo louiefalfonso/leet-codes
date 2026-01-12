f4d9c54c45c5773e7fca4a12733f00b692aaded2
Louie Alfonso <lfriasalfonso@gmail.com>
2026-01-12 17:46:43 +0000

    Leetcode Jan 12 2026

---
+++ python/Minimum Time Visiting All Points.py
@@ -0,0 +1,7 @@
+# Minimum Time Visiting All Points - 1266
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        return sum(
+            max(abs(p1[0] - p2[0]), abs(p1[1] - p2[1])) for p1, p2 in pairwise(points)
+        )
