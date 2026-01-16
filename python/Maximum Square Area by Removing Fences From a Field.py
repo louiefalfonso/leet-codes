4589f63b9500c5585529eddfedb3abeed2027d1c
Louie Alfonso <lfriasalfonso@gmail.com>
2026-01-16 21:20:20 +0000

    Leetcode Jan 16 2026

---
+++ python/Maximum Square Area by Removing Fences From a Field.py
@@ -0,0 +1,16 @@
+# Maximum Square Area by Removing Fences From a Field - 2975
+
+class Solution:
+    def maximizeSquareArea(
+        self, m: int, n: int, hFences: List[int], vFences: List[int]
+    ) -> int:
+        def f(nums: List[int], k: int) -> Set[int]:
+            nums.extend([1, k])
+            nums.sort()
+            return {b - a for a, b in combinations(nums, 2)}
+
+        mod = 10**9 + 7
+        hs = f(hFences, m)
+        vs = f(vFences, n)
+        ans = max(hs & vs, default=0)
+        return ans**2 % mod if ans else -1
