6d10c4655f69ac2a244806b97e56aa301582f996
Louie Alfonso <lfriasalfonso@gmail.com>
2026-01-26 20:14:51 +0000

    Leetcode Jan 26 2026

---
+++ python/Minimum Absolute Difference.py
@@ -0,0 +1,7 @@
+# Minimum Absolute Difference - 1200
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        mi = min(b - a for a, b in pairwise(arr))
+        return [[a, b] for a, b in pairwise(arr) if b - a == mi]
