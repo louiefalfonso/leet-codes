b03dc62e8c237b0d8c477e02cdd2a9baeab2df47
Louie Alfonso <lfriasalfonso@gmail.com>
2026-02-17 21:46:04 +0000

    Leetcode Feb 17 2026

---
+++ java/Binary Watch.java
@@ -0,0 +1,18 @@
+// Binary Watch - 401
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 60; ++j) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == turnedOn) {
+                    ans.add(String.format("%d:%02d", i, j));
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ python/Binary Watch.py
@@ -0,0 +1,10 @@
+# Binary Watch - 401
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        return [
+            '{:d}:{:02d}'.format(i, j)
+            for i in range(12)
+            for j in range(60)
+            if (bin(i) + bin(j)).count('1') == turnedOn
+        ]
