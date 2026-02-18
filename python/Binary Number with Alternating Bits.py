923379cc70fdc2aa5990b36bef85a0b172a2ae21
Louie Alfonso <lfriasalfonso@gmail.com>
2026-02-18 19:32:03 +0000

    Leetcode Feb 18 2026

---
+++ python/Binary Number with Alternating Bits.py
@@ -0,0 +1,12 @@
+#  Binary Number with Alternating Bits - 693
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        prev = -1
+        while n:
+            curr = n & 1
+            if prev == curr:
+                return False
+            prev = curr
+            n >>= 1
+        return True
