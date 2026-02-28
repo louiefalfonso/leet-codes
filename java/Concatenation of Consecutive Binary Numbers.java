7d9bd8af52a63ff508d35b28c8afcfa62851a669
Louie Alfonso <lfriasalfonso@gmail.com>
2026-02-28 23:05:28 +0000

    Leetcode Feb 28 2026

---
+++ java/Concatenation of Consecutive Binary Numbers.java
@@ -0,0 +1,12 @@
+// Concatenation of Consecutive Binary Numbers - 1680
+
+class Solution {
+    public int concatenatedBinary(int n) {
+        final int mod = (int) 1e9 + 7;
+        long ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            ans = (ans << (32 - Integer.numberOfLeadingZeros(i)) | i) % mod;
+        }
+        return (int) ans;
+    }
+}
---
+++ python/Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,9 @@
+# Concatenation of Consecutive Binary Numbers - 1680
+
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        mod = 10**9 + 7
+        ans = 0
+        for i in range(1, n + 1):
+            ans = (ans << i.bit_length() | i) % mod
+        return ans
