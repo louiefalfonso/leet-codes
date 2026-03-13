9744145f2af310ea66f492d138d55d09e3c9faa4
Louie Alfonso <lfriasalfonso@gmail.com>
2026-03-13 16:46:52 +0000

    Leetcode Mar 13 2026

---
+++ python/Minimum Number of Seconds to Make Mountain Height Zero.py
@@ -0,0 +1,11 @@
+# Minimum Number of Seconds to Make Mountain Height Zero - 3296
+
+class Solution:
+    def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
+        def check(t: int) -> bool:
+            h = 0
+            for wt in workerTimes:
+                h += int(sqrt(2 * t / wt + 1 / 4) - 1 / 2)
+            return h >= mountainHeight
+
+        return bisect_left(range(10**16), True, key=check)
