9aefb7a67c8e3886fbbb7270bc61a000ff3650bb
Louie Alfonso <lfriasalfonso@gmail.com>
2026-03-29 22:42:25 +0100

    Leetcode Mar 29 2026

---
+++ python/Check if Strings Can be Made Equal With Operations I.py
@@ -0,0 +1,7 @@
+# Check if Strings Can be Made Equal With Operations I - 2839
+
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        return Counter(s1[::2]) == Counter(s2[::2]) and Counter(s1[1::2]) == Counter(
+            s2[1::2]
+        )
