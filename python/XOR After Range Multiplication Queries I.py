8258c488640754e69c44e0238724edc6631a73ae
Louie Alfonso <lfriasalfonso@gmail.com>
2026-04-08 20:34:35 +0100

    Leetcode Apr 08 2026

---
+++ python/XOR After Range Multiplication Queries I.py
@@ -0,0 +1,9 @@
+# XOR After Range Multiplication Queries I - 3653
+
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        mod = 10**9 + 7
+        for l, r, k, v in queries:
+            for idx in range(l, r + 1, k):
+                nums[idx] = nums[idx] * v % mod
+        return reduce(xor, nums)
