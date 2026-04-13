24efb6c38d0ef37bbe33ecb893d056891f047fad
Louie Alfonso <lfriasalfonso@gmail.com>
2026-04-13 20:11:01 +0100

    Leetcode Apr 13 2026

---
+++ python/Minimum Distance to the Target Element.py
@@ -0,0 +1,4 @@
+# Minimum Distance to the Target Element - 1848
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        return min(abs(i - start) for i, x in enumerate(nums) if x == target)
