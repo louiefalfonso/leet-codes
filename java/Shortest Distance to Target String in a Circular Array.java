4f2acc84b4d4bd3578bd1fe32f0ca2f4edcc718e
Louie Alfonso <lfriasalfonso@gmail.com>
2026-04-15 20:56:27 +0100

    Leetcode Apr 15 2026

---
+++ java/Shortest Distance to Target String in a Circular Array.java
@@ -0,0 +1,15 @@
+// Shortest Distance to Target String in a Circular Array - 2515
+
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        int ans = n;
+        for (int i = 0; i < n; i++) {
+            if (words[i].equals(target)) {
+                int t = Math.abs(i - startIndex);
+                ans = Math.min(ans, Math.min(t, n - t));
+            }
+        }
+        return ans == n ? -1 : ans;
+    }
+}
---
+++ python/Shortest Distance to Target String in a Circular Array.py
@@ -0,0 +1,11 @@
+# Shortest Distance to Target String in a Circular Array - 2515
+
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        n = len(words)
+        ans = n
+        for i, w in enumerate(words):
+            if w == target:
+                t = abs(i - startIndex)
+                ans = min(ans, t, n - t)
+        return -1 if ans == n else ans
