b8e24b30dfdbf6a3591c0304297caad1cb58686e
Louie Alfonso <lfriasalfonso@gmail.com>
2026-04-19 21:58:12 +0100

    Leetcode Apr 19 2026

---
+++ python/Maximum Distance Between a Pair of Values.py
@@ -0,0 +1,10 @@
+# Maximum Distance Between a Pair of Values - 1855
+
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = 0
+        nums2 = nums2[::-1]
+        for i, v in enumerate(nums1):
+            j = len(nums2) - bisect_left(nums2, v) - 1
+            ans = max(ans, j - i)
+        return ans
