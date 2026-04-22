d4cec897f9ec58f5788a33081ee53ed91b78ccfc
Louie Alfonso <lfriasalfonso@gmail.com>
2026-04-22 20:06:55 +0100

    Leetcode Apr 22 2026

---
+++ python/Words Within Two Edits of Dictionary.py
@@ -0,0 +1,11 @@
+# Words Within Two Edits of Dictionary - 2452
+
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        ans = []
+        for s in queries:
+            for t in dictionary:
+                if sum(a != b for a, b in zip(s, t)) < 3:
+                    ans.append(s)
+                    break
+        return ans
